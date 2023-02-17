3cf0870332b042df4d549c49526e90fb4eb53459
Pankaj <pankajsonihr@gmail.com>
2023-02-17 18:35:49 -0500

    Initial commit

---
+++ app/src/main/java/com/Score/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.Score
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        /*val spinner = findViewById<Spinner>(R.id.spinner1)
+        val arrayList: ArrayList<String> = ArrayList()
+        arrayList.add("2");
+        arrayList.add("3");
+        arrayList.add("4");
+        arrayList.add("5");
+        arrayList.add("6");
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, arrayList)
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)*/
+    }
+}
